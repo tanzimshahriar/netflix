504435380e2b271fe8df2fd23f04fb7fd4b4b0e0
Tanzim Shahriar <tanzimshahriarfahim@gmail.com>
2023-07-14 20:21:38 +1000

    add drizzle locally and health endpoint

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,7 +4,7 @@ import 'dotenv/config'
 if (!process.env.POSTGRES_URL) {
   throw new Error('POSTGRES_URL is missing')
 }
-
+console.log('POSTGRES URL IS: ', process.env.POSTGRES_URL)
 export default {
   schema: './src/db/schema.ts',
   out: './src/db/migrations',
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,7 @@
 import { sql } from '@vercel/postgres'
 import { drizzle } from 'drizzle-orm/vercel-postgres'
+// import { migrate } from 'drizzle-orm/vercel-postgres/migrator'
 
 export const db = drizzle(sql)
+
+// migrate(db, { migrationsFolder: './drizzle' })
