e0024f41fc9fc85cdd9666fc591113573d7765fa
Tanzim Shahriar <tanzimshahriarfahim@gmail.com>
2023-11-09 13:30:19 +1100

    add search api endpoint and mobile nav bar

---
+++ src/app/api/search/route.ts
@@ -0,0 +1,69 @@
+import { getSession } from '@/lib/auth/session'
+import { NextResponse } from 'next/server'
+import { z } from 'zod'
+
+const searchSchema = z.object({
+  keyword: z.string().nonempty(),
+})
+
+export async function GET(req: Request) {
+  const session = await getSession()
+  if (null === session) {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
+  }
+  const { searchParams } = new URL(req.url)
+  const keyword = searchParams.get('keyword')
+  try {
+    searchSchema.parse({ keyword })
+  } catch (error) {
+    return new NextResponse(
+      JSON.stringify({
+        error,
+      }),
+      { status: 400 },
+    )
+  }
+  try {
+    const movies = await fetch(
+      `https://api.themoviedb.org/3/search/movie?query=${keyword}&page=1`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.TMDB_TOKEN}`,
+        },
+      },
+    )
+    const moviesResult = await movies.json()
+    const tvShows = await fetch(
+      `https://api.themoviedb.org/3/search/tv?query=${keyword}&page=1`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.TMDB_TOKEN}`,
+        },
+      },
+    )
+    const tvShowsResult = await tvShows.json()
+
+    moviesResult.results.forEach((r: any) => (r.media_type = 'movie'))
+    tvShowsResult.results.forEach((r: any) => (r.media_type = 'tv'))
+
+    let finalResult = [...moviesResult.results, tvShowsResult.results]
+    finalResult.sort((a, b) => {
+      const first = a.title || a.name
+      const second = b.title || b.name
+      return second.localeCompare(first)
+    })
+
+    return new NextResponse(
+      JSON.stringify({
+        data: finalResult,
+      }),
+    )
+  } catch (error: any) {
+    return new NextResponse(
+      JSON.stringify({
+        error,
+      }),
+      { status: 500 },
+    )
+  }
+}
--- src/components/Browse/Navbar.tsx
+++ src/components/Browse/Navbar.tsx
@@ -3,6 +3,7 @@ import Logo from '../Logo'
 import { headers } from 'next/headers'
 import LogoutButton from '../Ui/LogoutButton'
 import NavbarWrapper from './NavbarWrapper'
+import Image from 'next/image'
 
 const links = [
   {
@@ -45,10 +46,20 @@ const Navbar = () => {
             <div className="w-24 lg:w-32">
               <Logo />
             </div>
-            <div className="hidden items-center justify-center text-xs lg:flex xl:text-sm">
+            <div className="group absolute left-36 top-0 flex flex-col text-xs lg:static lg:flex-row lg:items-center lg:justify-center xl:text-sm">
+              <div className="flex items-center justify-center  gap-1 p-2 pt-6 text-sm lg:hidden">
+                <div className="text-xs">Browse</div>
+                <Image
+                  src="/down.svg"
+                  width={10}
+                  height={10}
+                  alt="down"
+                  className="invert"
+                />
+              </div>
               {links.map((link) => (
                 <Link
-                  className={`p-4 duration-500 hover:text-zinc-400 ${
+                  className={`hidden bg-zinc-800 p-2 duration-500 group-hover:block hover:text-zinc-400 lg:block lg:p-4 ${
                     pathname === link.url ? 'font-medium' : 'font-light'
                   }`}
                   key={link.url}
